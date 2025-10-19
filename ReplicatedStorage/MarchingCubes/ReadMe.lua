e85ca43c223285b35d57166f3d297b541297497a
l7myfr <progforlemy@gmail.com>
2025-10-19 21:11:48 +0400

    Updated ReplicatedStorage/MarchingCubes/ReadMe.lua

--- ReplicatedStorage/MarchingCubes/ReadMe.lua
+++ ReplicatedStorage/MarchingCubes/ReadMe.lua
@@ -4,6 +4,9 @@
 showcase : 
 youtube.com/watch?v=DKEdPnLmEAo&list=PLdnew4_vXxjmZIz_x7UI4geo5ViV4xXY6&index=3
 
+resource/inspo:
+https://www.youtube.com/watch?v=M3iI2l0ltbE&t=178s
+
 the script is expected to lag if you load a big range.
 like 100x250x250 for around maybe 15-30 seconds.
 this time will increase as the range gets bigger.
