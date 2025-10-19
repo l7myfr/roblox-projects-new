6fe145aaed241d9cb90c7aadd7e551ade967e7ba
l7myfr <progforlemy@gmail.com>
2025-10-19 21:26:01 +0400

    Updated ReplicatedStorage/Boids/ReadMe.lua

---
+++ ReplicatedStorage/Boids/ReadMe.lua
@@ -0,0 +1,15 @@
+-- @ScriptType: Script
+--[[
+janitor module isnt mine (obv)
+showcase : youtube.com/watch?v=9L586kAtrXM&list=PLdnew4_vXxjmZIz_x7UI4geo5ViV4xXY6&index=2
+inspo/resource : youtube.com/watch?v=bqtqltqcQhw&t=349s
+
+
+this is an abbonded project. i really liked an idea of it
+wanted to add multi threading. never got to it got too busy and now i am not as interested in this
+
+this is made for fun. not for a game.
+should not be used for a game
+
+
+]]
