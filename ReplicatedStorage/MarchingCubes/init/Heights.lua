44d8b069aae1b2d837802b265327aa6df544fbb8
l7myfr <progforlemy@gmail.com>
2025-10-19 21:07:12 +0400

    Updated ReplicatedStorage/MarchingCubes/init/Heights.lua

--- ReplicatedStorage/MarchingCubes/init/Heights.lua
+++ ReplicatedStorage/MarchingCubes/init/Heights.lua
@@ -9,6 +9,7 @@ function TerrainGenerator.Generate(self)
 	local Settings = self.Settings
 	local TotalWidthX = Settings.WidthX
 	local NumChunks = math.ceil((TotalWidthX + 1) / ChunkSize)
+	-- we js spliting up the workload hooray
 	print("Number of chunks/threads working ", NumChunks)
 	for i = 0, NumChunks - 1 do
 		task.spawn(function()
@@ -28,60 +29,6 @@ function TerrainGenerator.Generate(self)
 		end)
 	end
 end
-
---local MarchCubes = require(ReplicatedStorage.init.MarchCubes)
-
---function TerrainGenerator.Generate(self)
---	local Settings = self.Settings
---	local Height = Settings.Height
---	local WidthX = Settings.WidthX
---	local WidthZ = Settings.WidthZ
---	local SurfaceThreshold = Settings.SurfaceThreshold
---	local NoiseScale = Settings.NoiseScale
---	self.Heights = {}
---	local Heights = self.Heights
-
---	local pos = Settings.Position or Vector3.zero
---	for x = 1, WidthX + 1 do
---		local globalX = (x - 1) + pos.X
---		if not Heights[x] then Heights[x] = {} end
-
---		for y = 1, Height + 1 do
---			local globalY = (y - 1) + pos.Y
---			if not Heights[x][y] then Heights[x][y] = {} end
-
---			for z = 1, WidthZ + 1 do
-
-
-
---				local globalZ = (z - 1) + pos.Z
-
---				local worldX = globalX * NoiseScale
---				local worldY = globalY * NoiseScale
---				local worldZ = globalZ * NoiseScale
-
---				local NoiseValue = math.noise(worldX, worldY, worldZ)
---				local CHeight = Height * NoiseValue
-
---				local DistanceToSurface
---				if y <= CHeight - SurfaceThreshold then
---					DistanceToSurface = 0
---				elseif y > CHeight + SurfaceThreshold then
---					DistanceToSurface = 1
---				elseif y > CHeight then
---					DistanceToSurface = y - CHeight
---				else
---					DistanceToSurface = CHeight - y
---				end
---				Heights[x][y][z] = DistanceToSurface
---			end
---		end
---	end
---	self.Settings.StartX = 1
---	self.Settings.EndX = WidthX
---	MarchCubes.MarchCubes(self)
---end
-
 
 
 return TerrainGenerator
