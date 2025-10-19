ade3ecd4143e40001ec1cbb654dd45036586abd2
l7myfr <progforlemy@gmail.com>
2025-10-19 21:07:14 +0400

    Updated ReplicatedStorage/MarchingCubes/ReadMe.lua

---
+++ ReplicatedStorage/MarchingCubes/ReadMe.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: Script
+--[[
+
+showcase : 
+youtube.com/watch?v=DKEdPnLmEAo&list=PLdnew4_vXxjmZIz_x7UI4geo5ViV4xXY6&index=3
+
+the script is expected to lag if you load a big range.
+like 100x250x250 for around maybe 15-30 seconds.
+this time will increase as the range gets bigger.
+the lag is caused by the script caluclatating the terrain.
+it is recommended to load small range
+this system was made as a small project not tbe used in a game just for fun.
+]]
