04fac23652a526af448177039006f462f2399372
l7myfr <progforlemy@gmail.com>
2025-10-19 21:07:12 +0400

    Updated ReplicatedStorage/MarchingCubes/init.lua

--- ReplicatedStorage/MarchingCubes/init.lua
+++ ReplicatedStorage/MarchingCubes/init.lua
@@ -9,10 +9,10 @@ local RenderMarchedCube = require(script.RenderMarchedCube)
 function Marching.init(settings)
 	local self = setmetatable({}, Marching)
 	self.Settings = {
-		Height = settings.Heights or 500,
-		WidthX = settings.Width or 500, 
-		WidthZ = settings.Width or 500, 
-		NoiseScale = settings.NoiseScale or 0.01,
+		Height = settings.Heights or 100,
+		WidthX = settings.Width or 250, 
+		WidthZ = settings.Width or 250, 
+		NoiseScale = settings.NoiseScale or 0.05,
 		SurfaceThreshold = settings.SurfaceThreshold or 0.5,
 		DetailScale =  settings.DetailScale or 1,
 		CellSize = settings.CellSize or 1,
