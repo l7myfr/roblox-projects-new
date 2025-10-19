990412da8ab9a735ba8c356ae8869637d2f98403
l7myfr <progforlemy@gmail.com>
2025-10-19 22:23:49 +0400

    Updated ReplicatedStorage/Voxel/ReadMe.lua

---
+++ ReplicatedStorage/Voxel/ReadMe.lua
@@ -0,0 +1,11 @@
+-- @ScriptType: Script
+--[[
+
+one of my projects taht was ACTUALLY used in a game.
+though this a remastered module of the old module that could be used in a game
+this module can probably be also used in a game. idk maybe. not really upto make a game from them
+
+inspos: realm rampage, jujutsu shenanignes (sorry dont know how to spell it)
+
+network module, partpooling, signal made by me. not for this project js for general use
+]]
